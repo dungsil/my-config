be8f9f7d8d13425ad38e046092f572152baefb9c
김용건 (Kim Younggeon) <jonathan@lsware.com>
2020-10-14 14:15:53 +0900

    feat(commitlint-config): commitlint 설정 프리셋 추가

---
+++ packages/commitlint-config/README.md
@@ -0,0 +1,7 @@
+# @dungsil/commitlint-config
+commitlint 설정
+
+## `@commitlint/config-conventional`과의 차이
+1. 커밋 메시지에 대소문자 구분을 하지 않음
+2. 헤더 메시지 길이를 120자로 증가
+3. 본문/푸터 메시지 길이를 180자로 증가
---
+++ packages/commitlint-config/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [0],
+    'header-max-length': [2, 'always', 120],
+    'body-max-line-length': [2, 'always', 180],
+    'footer-max-line-length': [2, 'always', 180],
+  },
+}
