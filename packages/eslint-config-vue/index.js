6d2e17dfd304144d98d6f26d37397445e0c67991
김용건 (Kim Younggeon) <jonathan@lsware.com>
2020-10-14 14:29:23 +0900

    feat(eslint-config-vue): Vue.js 용 ESLint 설정 추가

---
+++ packages/eslint-config-vue/index.js
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: ['@eyesvault', 'plugin:vue/recommended', 'prettier', 'prettier/vue'],
+  parser: 'vue-eslint-parser',
+  plugins: ['vue'],
+  rules: {
+    /**********************/
+    /*     Vue Rules      */
+    /**********************/
+
+    // Disable template errors regarding invalid end tags
+    'vue/no-parsing-error': [
+      'error',
+      {
+        'x-invalid-end-tag': false,
+      },
+    ],
+
+    // Maximum 5 attributes per line instead of one
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: 5,
+      },
+    ],
+
+    'vue/html-indent': 'off',
+  },
+}
