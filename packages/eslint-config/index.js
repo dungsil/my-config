6146a799a241647aedb6a8456741f066af4f4ea4
김용건 (Kim Younggeon) <jonathan@lsware.com>
2020-10-14 14:31:06 +0900

    feat(eslint-config): Typescript용 ESLint 설정 추가

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: [
+    '@eyesvault/eslint-config-base',
+
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/member-delimiter-style': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { args: 'all', argsIgnorePattern: '^_' }],
+  },
+}
