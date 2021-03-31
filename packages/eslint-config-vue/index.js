e5c84523442238086f094db2d96f79cef92fcaf5
김용건 (Kim Younggeon) <mail@kyg.kr>
2021-03-31 14:14:30 +0900

    fix(eslint-config-vue): prettier 제거

--- packages/eslint-config-vue/index.js
+++ packages/eslint-config-vue/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['plugin:vue/recommended', 'prettier', 'prettier/vue'],
+  extends: ['plugin:vue/recommended'],
   parser: 'vue-eslint-parser',
   plugins: ['vue'],
   rules: {
